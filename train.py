e7ed1697d5e1506fef3409f003528a16aee27e05
SilvrDuck <thibault.viglino@epfl.ch>
2018-10-19 14:24:06 +0200

    minor

--- train.py
+++ train.py
@@ -311,7 +311,6 @@ if __name__ == '__main__':
 
             if args.cuda:
                 inputs = inputs.cuda()
-                target_accents = target_accents.cuda()
 
             if args.model == 'deepspeech':
                 out, output_sizes = model(inputs, input_sizes)
@@ -324,7 +323,7 @@ if __name__ == '__main__':
                 side_out = side_out.transpose(0, 1)  # TxNxH
             
 #                dummy_out = torch.sum(side_out, dim=1).long()
-                loss = criterion((out, targets, output_sizes, target_sizes), (side_out, target_accents))
+                loss = criterion((out, targets.cpu(), output_sizes, target_sizes), (side_out, target_accents))
             
             loss = loss / inputs.size(0)  # average the loss by minibatch
 
